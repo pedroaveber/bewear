df7d9a7058dc8968f39a454b93f8d45e458e8660
pedroaveber <pedro.veber@outlook.com>
2025-08-05 18:20:39 -0300

    chore: add better auth tables

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,8 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
+// biome-ignore lint/performance/noNamespaceImport: Only used once
+import * as schema from './schema';
 
-export const db = drizzle(process.env.DATABASE_URL || '');
+export const db = drizzle(process.env.DATABASE_URL || '', {
+  schema,
+});
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,58 @@
 import { relations } from 'drizzle-orm';
-import { integer, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import {
+  boolean,
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users', {
-  id: uuid('id').primaryKey().defaultRandom(),
+  id: text('id').primaryKey(),
   name: text('name').notNull(),
+  email: text('email').notNull().unique(),
+  emailVerified: boolean('email_verified')
+    .$defaultFn(() => false)
+    .notNull(),
+  image: text('image'),
+  createdAt: timestamp('created_at')
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .notNull(),
+  updatedAt: timestamp('updated_at')
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .notNull(),
+});
+
+export const sessionsTable = pgTable('sessions', {
+  id: text('id').primaryKey(),
+  expiresAt: timestamp('expires_at').notNull(),
+  token: text('token').notNull().unique(),
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at').notNull(),
+  ipAddress: text('ip_address'),
+  userAgent: text('user_agent'),
+  userId: text('user_id')
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+});
+
+export const accountsTable = pgTable('accounts', {
+  id: text('id').primaryKey(),
+  accountId: text('account_id').notNull(),
+  providerId: text('provider_id').notNull(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  accessToken: text('access_token'),
+  refreshToken: text('refresh_token'),
+  idToken: text('id_token'),
+  accessTokenExpiresAt: timestamp('access_token_expires_at'),
+  refreshTokenExpiresAt: timestamp('refresh_token_expires_at'),
+  scope: text('scope'),
+  password: text('password'),
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at').notNull(),
 });
 
 export const categoriesTable = pgTable('categories', {
@@ -19,12 +68,9 @@ export const categoriesRelations = relations(categoriesTable, ({ many }) => ({
 
 export const productsTable = pgTable('products', {
   id: uuid('id').primaryKey().defaultRandom(),
-  categoryId: uuid('category_id')
-    .references(() => categoriesTable.id, {
-      onDelete: 'cascade',
-      onUpdate: 'cascade',
-    })
-    .notNull(),
+  categoryId: uuid('category_id').references(() => categoriesTable.id, {
+    onDelete: 'set null',
+  }),
   name: text('name').notNull(),
   slug: text('slug').notNull().unique(),
   description: text('description').notNull(),
@@ -44,7 +90,6 @@ export const productVariantsTable = pgTable('product_variants', {
   productId: uuid('product_id')
     .references(() => productsTable.id, {
       onDelete: 'cascade',
-      onUpdate: 'cascade',
     })
     .notNull(),
   name: text('name').notNull(),
